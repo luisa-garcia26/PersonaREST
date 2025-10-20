6b12123d099158e5b3f5647de11aabac8902e312
luisa-garcia26 <luisa-garcia2@upc.edu.co>
2025-10-20 14:37:02 -0500

    Add files via upload

---
+++ PersonaREST/src/main/java/com/mycompany/personarest/ApplicationConfig.java
@@ -0,0 +1,7 @@
+package com.mycompany.personarest;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+@ApplicationPath("/api")
+public class ApplicationConfig extends Application {}
